c1d75cfd5e2cc3dc96cf873930724b71e84273cf
0neAnd0nlyKen <kendrickraphael@gmail.com>
2026-05-05 22:09:50 +0800

    audio slider components

--- scenes/mainmenu.gd
+++ scenes/mainmenu.gd
@@ -1,8 +1,14 @@
 extends Control
 
+@export var masterSlider: HSlider
+@export var musicSlider: HSlider
+@export var sfxSlider: HSlider
+var master_bus_index: int
+var music_bus_index: int
+var sfx_bus_index: int
 
 func _ready() -> void:
-	
+	readyAudioSliders()
 	#play the songs, load settings, and load the fucking assets, also load the save files
 	print(DisplayServer.get_display_safe_area())
 
@@ -31,3 +37,27 @@ func _on_no_pressed() -> void:
 
 func _on_yes_pressed() -> void:
 	get_tree().quit()
+	
+
+func readyAudioSliders() -> void:
+	masterSlider.value = db_to_linear(AudioServer.get_bus_volume_db(master_bus_index))
+	musicSlider.value = db_to_linear(AudioServer.get_bus_volume_db(music_bus_index))
+	sfxSlider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus_index))
+
+func _on_temp_master_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		master_bus_index,
+		linear_to_db(value)
+	)
+
+func _on_temp_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		music_bus_index,
+		linear_to_db(value)
+	)
+
+func _on_temp_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		sfx_bus_index,
+		linear_to_db(value)
+	)
