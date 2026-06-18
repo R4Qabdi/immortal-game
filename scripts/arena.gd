20523eaef0aad54322fba55887d020cda89a8b94
shi-reker <shypherosira@gmail.com>
2026-06-18 19:01:06 +0800

    groundwork for scene

--- scripts/arena.gd
+++ scripts/arena.gd
@@ -11,22 +11,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var prefTile:int = 0
-	for i in range(8):
-		for j in range(8):
-			if prefTile == 0:
-				#tile.color = Color.WHITE
-				prefTile = 1
-				# spawn tile
-				# tile.pos.x = tile.size.x * j
-				# tile.pos.y = tile.size.x * -1
-			else:
-				#tile.color = Color.BLACK
-				prefTile = 0
-				# spawn tile
-				# tile.pos.x = tile.size.x * j
-				# tile.pos.y = tile.size.x * -1
-		
+	pass
 
 func _on_texture_button_pressed() -> void:
 	ShopInstructions.data = {
