b89cb9fa30d056bfea1be8a5291a9f3c42130c73
0neAnd0nlyKen <141936774+0neAnd0nlyKen@users.noreply.github.com>
2026-06-30 20:35:46 +0800

    Revert "Merge branch 'main' into cardDetails-main-merge"
    This reverts commit 35974e8c5d94985079fe86e63f222d4f4343be10, reversing
    changes made to 0cdd7102ab50f7c7cce70e48509acdc842e6d129.

--- scenes/squares/square.gd
+++ scenes/squares/square.gd
@@ -76,15 +76,13 @@ func set_preview(is_preview: bool) -> void:
 		tile.modulate = Color(1.0, 1.0, 1.0, 1.0)
 
 func _on_area_mouse_entered() -> void:
-	var current_color = $tile.modulate
-	$tile.modulate = Color.from_rgba8(135, 206, 250, current_color.a*255)
+	$tile.modulate = Color.LIGHT_BLUE
 	is_hovered = true
 	if board:
 		board.hovered_square = self # Catat kotak aktif yang di-hover mouse ke Board
 
 func _on_area_mouse_exited() -> void:
-	var current_color = $tile.modulate
-	$tile.modulate = Color.from_rgba8(255, 255, 255, current_color.a*255)
+	$tile.modulate = Color.WHITE
 	is_hovered = false
 	if board and board.hovered_square == self:
 		board.hovered_square = null # Hapus dari catatan Board jika keluar kotakss
